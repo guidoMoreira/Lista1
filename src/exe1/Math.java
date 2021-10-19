6a8c1fea04835cee39df26923f27e3ea4d597b17
guidoMoreira <guidomoreira@alunos.utfpr.edu.br>
2021-10-19 14:12:41 -0300

    math mudança

--- src/exe1/Math.java
+++ src/exe1/Math.java
@@ -11,5 +11,31 @@ public class Math {
     public float Abs(){
         return this.x + this.y;
     }
-
+    public float Ceil(){
+        return this.x + this.y;
+    }
+    public float Cos(){
+        return this.x + this.y;
+    }
+    public float Exp(){
+        return this.x + this.y;
+    }
+    public float Floor(){
+        return this.x + this.y;
+    }
+    public float Log(){
+        return this.x + this.y;
+    }
+    public float Max(){
+        return this.x + this.y;
+    }
+    public float Min(){
+        return this.x + this.y;
+    }
+    public float Pow(){
+        return this.x + this.y;
+    }
+    public float Sqrt(){
+        return x;
+    }
 }
