e24f13fe8d6433734dbbc7efbcc4a6618e81e673
guidoMoreira <guidomoreira@alunos.utfpr.edu.br>
2021-10-19 17:45:40 -0300

    completei exercicio 3 e 4

--- src/exe3/Estacionamento.java
+++ src/exe3/Estacionamento.java
@@ -8,16 +8,20 @@ public class Estacionamento {
     }
 
     public static void main(String[] args) {
-        ArrayList<Vaga> Vagas = new ArrayList<>();
-        Vagas.add(new Vaga("Carro1", "12345789", 10, 11));
-        Vagas.add(new Vaga("Carro3", "12345789", 8.5, 8.9));
-        Vagas.add(new Vaga("Moto", "12345789", 18, 21));
-        for(int i = 0; i < Vagas.size(); i++){
-        System.out.println("O Preço pago por " + Vagas.get(i).gettipo() + " R$" + Vagas.get(i).gerPreco());
+        //Parte exercicio 3
+        ArrayList<Vaga> Estacionamento1 = new ArrayList<>();
+        Estacionamento1.add(new Vaga("Carro1", "12345789", 10, 11));
+        Estacionamento1.add(new Vaga("Carro3", "12345789", 8.5, 8.9));
+        Estacionamento1.add(new Vaga("Moto", "12345789", 18, 21));
+        for(int i = 0; i < Estacionamento1.size(); i++) {
+            System.out.println("O Preço pago por " + Estacionamento1.get(i).gettipo() + " R$" + Estacionamento1.get(i).gerPreco());
+        }
+        //Incluindo funcionalidades da classe exercicio4
+       Exercicio4 MenuRegistrar = new Exercicio4();
+       ArrayList<Vaga> Estacionamento2 = MenuRegistrar.AdicionarReg();
+       for(int i = 0; i < Estacionamento2.size(); i++) {
+           System.out.println("O Preço pago por " + Estacionamento2.get(i).gettipo() + " R$" + Estacionamento2.get(i).gerPreco());
+       }
     }
-
-    }
-
-
 
 }
--- src/exe3/Exercicio4.java
+++ src/exe3/Exercicio4.java
@@ -1,11 +1,28 @@
 package exe3;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 //Exercicio 4
-//Essa classe permite que o programa adicione e remova registros de veiculos que usaram o estacionamento
+//Essa classe permite que o usuario digite os registros e adicione quantos quiser
 public class Exercicio4 {
     public Exercicio4(){
     }
-    public void func(){
-        System.out.println("Teste");
+    public ArrayList<Vaga> AdicionarReg(){
+        ArrayList<Vaga> Estacionamento = new ArrayList<>();
+        int aux = 0;
+        while(aux == 0) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite o tipo do veiculo: ");
+            String tipVeic = scan.nextLine();
+            System.out.println("Digite a placa do veiculo: ");
+            String Placa = scan.nextLine();
+            System.out.println("Digite a hora de chegada do veiculo: ");
+            Float Hin = scan.nextFloat();
+            System.out.println("Digite hora de saida do veiculo: ");
+            Float Hout = scan.nextFloat();
+            Estacionamento.add(new Vaga(tipVeic,Placa,Hin,Hout));
+            System.out.println("Adicionar outro veiculo?\n0 - Sim\n1 - Não");
+            aux = scan.nextInt();
+        }
+        return Estacionamento;
     }
 }
